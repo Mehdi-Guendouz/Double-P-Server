6897ff37d87d45bbdc85e0851ac07ad5f2d0f059
Guendouz mehdi <mahdign12@gmail.com>
2024-12-22 03:25:59 +0100

    debuging

--- src/main.ts
+++ src/main.ts
@@ -12,8 +12,12 @@ async function bootstrap() {
   );
   app.setGlobalPrefix('api');
   app.enableCors({
-    origin: [process.env.CLIENT_URL, 'https://double-p-app.vercel.app/'],
+    origin: [process.env.CLIENT_URL],
     credentials: true,
+  });
+  app.use((req, res, next) => {
+    console.log('Origin:', req.headers.origin);
+    next();
   });
   await app.listen(5000);
 }
